64dd22311c3a908e43d98f1d074c8511de0913b5
anasbusytech <anas24.busytech@gmail.com>
2026-07-03 17:50:55 +0500

    database server.js file upadte

--- app/db.server.js
+++ app/db.server.js
@@ -1,18 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-console.log("NODE_ENV:", process.env.NODE_ENV);
-
-const dbUrl = process.env.DATABASE_URL;
-
-if (dbUrl) {
-  const url = new URL(dbUrl);
-
-  console.log("DB HOST:", url.hostname);
-  console.log("DB NAME:", url.pathname);
-} else {
-  console.log("DATABASE_URL is missing");
-}
-
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
     global.prismaGlobal = new PrismaClient();
